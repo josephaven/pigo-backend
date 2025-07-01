b8826348b9a600010fef018c6e41b4b9f371de7e
josephaven <jr457944@gmail.com>
2025-07-01 01:08:04 -0600

    Configuración inicial de rutas y estilos base en Tailwind

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,15 @@ Route::middleware([
     config('jetstream.auth_session'),
     'verified',
 ])->group(function () {
+
+    // Ruta general
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
+
+    // Ruta protegida solo para Jefe
+    Route::get('/admin', function () {
+        return 'Bienvenido Jefe';
+    })->middleware('rol:Jefe');
 });
+
